066e01d972006ac8daf56c6ee510fc3efdffceab
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-08-28 17:28:14 +0800

    [Rank][Jack] refactor: rename method addResult to addResultByHistory

--- src/rank.js
+++ src/rank.js
@@ -17,10 +17,8 @@ function hasChina (history) {
 }
 
 function captainHistoryRisk (voyage, history) {
-  let result = 1;
-  if (history.length < 5) {
-    result += 4;
-  }
+  let result = 5;
+  result = addResult(history, 4, -4, result);
   result += history.filter(v => v.profit < 0).length;
   if (isInChinaAndHasHistory(voyage, history)) {
     result -= 2;
@@ -35,23 +33,26 @@ function voyageProfitFactor (voyage, history) {
     }
   if (isInChinaAndHasHistory(voyage, history)) {
     result += 3;
-    if (history.length > 10) {
-      result += 1;
-    }
+    result = addResult(history, 10, 1, result);
     if (voyage.length > 12 && voyage.length < 18) {
       result += 1;
     }
   }
   else {
-    if (history.length > 8) {
-      result += 1;
-    }
+    result = addResult(history, 8, 1, result);
     if (voyage.length > 14) {
       result -= 1;
     }
   }
   return result;
 }
+
+function addResult(history, boundaryValue, increment, result){
+    if(history.length > boundaryValue)
+        result += increment;
+    return result;
+}
+
 
 function rating (voyage, history) {
   const vpf = voyageProfitFactor(voyage, history);
